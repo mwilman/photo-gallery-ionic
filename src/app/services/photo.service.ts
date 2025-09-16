f22088c0ef079c1ed71c3ff4ef1f7c8dbf104b2f
Maxim Wilman <mwilman@brockhaus-ag.de>
2025-09-16 12:47:53 +0200

    Add PhotoService to capture and display photos in Tab 2

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {Camera, CameraResultType, CameraSource} from "@capacitor/camera";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public photos: UserPhoto[] = [];
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath!
+    })
+  }
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath: string;
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonIcon} from '@ionic/angular/standalone';
+import {Component, inject} from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFabButton,
+  IonFab,
+  IonIcon,
+  IonRow, IonGrid, IonImg, IonCol
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {PhotoService} from "../services/photo.service";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonIcon]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonIcon, IonRow, IonGrid, IonImg, IonCol]
 })
 export class Tab2Page {
 
   constructor() {}
+  photoService = inject(PhotoService)
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery()
+  }
 }
